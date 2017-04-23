257b992825828b58297f5c04397bef0168557788
Senko Shirogane <senko shirogane>
2017-04-22 21:33:29 -0500

    Login más bonito x3

--- app/src/main/java/ipn/sofficcesware/com/mqst/MainActivity.java
+++ app/src/main/java/ipn/sofficcesware/com/mqst/MainActivity.java
@@ -5,12 +5,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import ipn.sofficcesware.com.mqst.administrador.Administrador;
 import ipn.sofficcesware.com.mqst.cliente.Cliente;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     Button IniciarSesion, Limpiar, IniciarSesion2;
+    EditText Usuario, Contrasena;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,10 +23,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         IniciarSesion.setOnClickListener(this);
         IniciarSesion2 = (Button) findViewById(R.id.btnIniciarSesion2);
         IniciarSesion2.setOnClickListener(this);
+        Limpiar = (Button) findViewById(R.id.btnLimpiarLogin);
+        Limpiar.setOnClickListener(this);
+
+        Usuario = (EditText) findViewById(R.id.usuarioLogin);
+        Contrasena = (EditText) findViewById(R.id.contrasenaLogin);
     }
 
     @Override
     public void onClick(View v) {
+        String usuario = Usuario.getText().toString();
+        String contrasena = Contrasena.getText().toString();
+
         switch(v.getId()){
             case R.id.btnIniciarSesion:
                 Intent intento1 = new Intent(MainActivity.this, Cliente.class);
@@ -33,6 +43,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.btnIniciarSesion2:
                 Intent intento2 = new Intent(MainActivity.this, Administrador.class);
                 startActivity(intento2);
+                break;
+            case R.id.btnLimpiarLogin:
+                Usuario.setText("");
+                Contrasena.setText("");
                 break;
         }
     }
